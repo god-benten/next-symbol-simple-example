c9076ce6886f4c0c6ca60a018908858e2facfb56
₿Ξ₦₮Ξ₦ <god@benten.tokyo>
2024-06-02 01:59:34 +0900

    introduce symbol-sdk and some dependencies, configure next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+      config.plugins.push(
+        new NodePolyfillPlugin({ excludeAliases: ['console'] }),
+        new webpack.NormalModuleReplacementPlugin(
+          /symbol-crypto-wasm-node/,
+          '../../../symbol-crypto-wasm-web/symbol_crypto_wasm.js'
+        ),
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer']
+        }),
+      );
+
+      config.experiments = { asyncWebAssembly: true, topLevelAwait: true, layers: true };
+      config.resolve.fallback = {
+        "crypto": 'crypto-browserify',
+        "stream": 'stream-browserify',
+      }
+      return config
+    }
+  };
 
 export default nextConfig;
